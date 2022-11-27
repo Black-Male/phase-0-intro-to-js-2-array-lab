044d07d25b779fcc782d37a19576000c6a252c9b
Black-Male <Gkaranja20@gmail.com>
2022-11-27 20:29:45 +0300

    Work Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function appendCat(name){
+   return [...cats,name]
+}
+function prependCat(name){
+    return[name, ...cats]
+}
+function removeLastCat(){
+    return [...cats.slice(0,2)]
+}
+function removeFirstCat(){
+    return [...cats.slice(1)]
+}
